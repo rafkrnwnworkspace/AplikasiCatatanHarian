088de0364fa4fa8797ded5776348688c2a9dcb89
rafkrnwnworkspace <136820310+rafkrnwnworkspace@users.noreply.github.com>
2024-11-11 13:49:24 +0800

    Menambahkan kodingan kedalam class Catatan

--- src/Catatan.java
+++ src/Catatan.java
@@ -1,13 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author rafkr
- */
 public class Catatan {
-    
+    private String judul;
+    private String isi;
+
+    public Catatan(String judul, String isi) {
+        this.judul = judul;
+        this.isi = isi;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getIsi() {
+        return isi;
+    }
+
+    public void setIsi(String isi) {
+        this.isi = isi;
+    }
+
+    @Override
+    public String toString() {
+        return judul;
+    }
 }
